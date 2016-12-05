92e568ae99da1feb446313f55237b4baffc77b43
Victor Nguyen <vdn140030@gmail.com>
2016-12-05 09:49:46 -0600

    Update RandomNumGuesser.py

--- RandomNumGuesser.py
+++ RandomNumGuesser.py
@@ -1,22 +1,22 @@
 import random	
 def main():
 	print ("Guess a number between 1 and 100: ")
-	randomNum = random.randint (1,100)			# Creates a random integer literal for every
-																	# run of the program
-	found = False											# Flag variable to see if
-																	# they guessed it
+	randomNum = random.randint (1,100)		# Creates a random integer literal for every
+							# run of the program
+	found = False					# Flag variable to see if
+							# they guessed it
 																	
-	while (found == False):							# !found does not work => use found == False 
-																	# as the alternative
-		userGuess = int(input ("Your guess: "))		# The input must be received as an integer
+	while (found == False):				# !found does not work => use found == False 
+							# as the alternative
+		userGuess = int(input ("Your guess: "))	# The input must be received as an integer
 																		# not a string
 		# Run through the guessing process
 		if (userGuess == randomNum):
 			print ("You got it!")
 			found = True
-		elif (userGuess < randomNum):					# Help the user increase the guess num
+		elif (userGuess < randomNum):		# Help the user increase the guess num
 			print ("Guess higher.")
-		else: #(userGuess < randomNum)				# Help the user decrease the guess num
+		else: #(userGuess < randomNum)		# Help the user decrease the guess num
 			print ("Guess lower.")
 	# Print congradulations and goodbye
 	print("Congrats! Thanks for playing the game!")
