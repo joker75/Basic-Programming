1611c01022a7d9b85cb298cfacf3d1e23e351fc0
Victor Nguyen <vdn140030@gmail.com>
2016-12-07 23:13:50 -0600

    Update Person.java

--- payroll-system/Person.java
+++ payroll-system/Person.java
@@ -5,14 +5,14 @@
 // should be an interface. Consider having an abstractPerson class
 public class Person {
 	// Defines private variables
-	private String id;					// Employee ID: anything really...
-	private String fullName;			// Full name: anything as well
+	private String id;			// Employee ID: Set of 8 integers
+	private String fullName;		// Full name: anything as well
 	protected static int hourlyPay;		// How much pay per hour: validate for positive #'s
 	protected static int workHours;		// How many hours: validate for positive #'s and
-										// max time (40 hours) overtime (58 hours)	
+						// max time (40 hours) overtime (58 hours)	
 	
 	// SETTER METHODS
-	public void setID(String input){this.id = input;}
+	public void setID(String input){this.id = input;}		
 	public void setBirthDate(String input){this.fullName = input;}
 	public void setHourlyPay(int pay){Person.hourlyPay = pay;}
 	public void setWorkHours(int hours){Person.workHours = hours;}
