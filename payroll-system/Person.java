112d7409d9c0c9068c261feb3aedb1ccf19c3a2b
Victor Nguyen <vdn140030@gmail.com>
2016-12-08 13:35:27 -0600

    Update Person.java

--- payroll-system/Person.java
+++ payroll-system/Person.java
@@ -1,21 +1,27 @@
-// This Person class will act as the 
-// main class from which all other 
-// classes will inherit from.
-
-// should be an interface. 
+/**************************************
+ * This interface has the basic methods
+ * for a person working at a company.
+ * No instances of this interface can
+ * be created.
+ *
+ * Created by:  Victor on 12/8/2016.
+ *              vdn140030
+ *              CS 2336.005
+ *              FALL 2016
+ *************************************/
 public interface Person {
-  	// SETTER METHODS
-	void setID(String input);
-	void setFullName(String input);
-	void setHourlyPay(int pay);
-	void setWorkHours(int hours);
-	
-	// GETTER METHODS
-	String getID();
-	String getFullName();
-	int getHourlyPay();
-	int getWorkHours():
-	
-	// Calculates total pay
-	int calc();
+    // SETTER METHODS
+    void setID(String input);
+    void setFullName(String input);
+    void setHourlyPay(int pay);
+    void setWorkHours(int hours);
+
+    // GETTER METHODS
+    String getID();
+    String getFullName();
+    int getHourlyPay();
+    int getWorkHours();
+
+    // Calculates total pay
+    int calc();
 }
