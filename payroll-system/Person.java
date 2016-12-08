4e96de5f7a7fd54c16610e35bcb0cdbd465cd20c
Victor Nguyen <vdn140030@gmail.com>
2016-12-08 12:37:41 -0600

    Update Person.java

--- payroll-system/Person.java
+++ payroll-system/Person.java
@@ -3,24 +3,19 @@
 // classes will inherit from.
 
 // should be an interface. 
-public class Person {
-	// Defines private variables
-	private String id;			// Employee ID: Set of 8 integers
-	private String fullName;		// Full name: anything as well
-	protected static int hourlyPay;		// How much pay per hour: validate for positive #'s
-	protected static int workHours;		// How many hours: validate for positive #'s and
-						// max time (40 hours) overtime (58 hours)	
-	
-	// SETTER METHODS
-	public void setID(String input){this.id = input;}		
-	public void setBirthDate(String input){this.fullName = input;}
-	public void setHourlyPay(int pay){Person.hourlyPay = pay;}
-	public void setWorkHours(int hours){Person.workHours = hours;}
+public interface Person {
+  	// SETTER METHODS
+	void setID(String input);
+	void setfullName(String input);
+	void setHourlyPay(int pay);
+	void setWorkHours(int hours);
 	
 	// GETTER METHODS
-	public String getID(){return this.id;}
-	public String getBirthDate(){return this.fullName; }
-	public int hourlyPay(){return Person.hourlyPay;}
-	public int getWorkHours(){return Person.workHours;}
-	public static int calc(){return hourlyPay * workHours;}
+	String getID();
+	String getfullName();
+	int gethourlyPay();
+	int getWorkHours():
+	
+	// Calculates total pay
+	int calc();
 }
