a45cbeada64ec0e201ed9fa6085f198d47c233d2
Victor Nguyen <vdn140030@gmail.com>
2016-12-08 12:41:03 -0600

    Update abstractPerson.java

--- payroll-system/abstractPerson.java
+++ payroll-system/abstractPerson.java
@@ -1,14 +1,16 @@
 public abstract abstractPerson implements Person{
   	// SETTER METHODS
 	public abstract void setID(String input);
-	public abstract void setBirthDate(String input);
+	public abstract void setFullName(String input);
 	public abstract void setHourlyPay(int pay);
 	public abstract void setWorkHours(int hours);
 	
 	// GETTER METHODS
 	public abstract String getID();
-	public abstract String getBirthDate();
-	public abstract int hourlyPay();
+	public abstract String getFullName();
+	public abstract int getHourlyPay();
 	public abstract int getWorkHours():
+	
+	// Calculates total pay
 	public abstract static int calc();
 }
