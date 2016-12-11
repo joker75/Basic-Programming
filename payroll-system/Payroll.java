73de75f9f4a10b731dbd8a8f2a94de70d297248f
Victor Nguyen <vdn140030@gmail.com>
2016-12-10 19:25:44 -0600

    Update Payroll.java

--- payroll-system/Payroll.java
+++ payroll-system/Payroll.java
@@ -9,6 +9,8 @@
  *********************************/
 import java.io.*;
 import java.lang.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 public class payroll {
     // Static scanner object for multiple uses
@@ -16,6 +18,7 @@ public class payroll {
 
     public static void main(String [] args){
         // Defines variables
+        Manager manager = new Manager();
         Employee employee_1 = new Employee();
         Employee employee_2 = new Employee();
         Employee employee_3 = new Employee();
@@ -28,7 +31,7 @@ public class payroll {
 
         //========MENU DISPLAY========\\
         do{
-            System.out.println("Welcome to the General Payroll System\nIt offers 3 options.\n"
+            System.out.println("Welcome to the General Payroll System\nIt offers 2 options.\n"
                     + "============================\n"
                     + "1. Document Employee Pay\n"
                     + "2. End the Program.\n"
@@ -38,6 +41,7 @@ public class payroll {
             try {
                 choice = input.nextInt();
                 choice = validate(choice);
+                input.nextLine();           // Discard current input
             }
 
             catch(InputMismatchException e){
@@ -48,10 +52,12 @@ public class payroll {
             switch (choice){
                 case 1:
              		// call input function
+                    INPUT(manager);
                     INPUT(workers);
 
               	    // call the file writing function
-                    write(workers);
+                    // INPUT(manager);
+                    write(workers, manager);
                     break;
                 case 2:
                	    break; // End the program
@@ -74,9 +80,75 @@ public class payroll {
         // Returns valid menu options
         return num;
     }
-    /*********************************************
-    * This function accepts input from the user. *
-    *********************************************/
+
+    /**********************************************
+    * These functions accept input from the user.
+    * Manager Function
+    **********************************************/
+    private static void INPUT(Manager manager1){
+        // Creates a dummy variable to validate input
+        String tempS;
+
+        // FULL NAME
+        System.out.println("\nPlease enter data for the Manager in charge of "
+                            + "these three employees.");
+        System.out.print("Full name: ");
+        tempS = input.nextLine();
+        manager1.setFullName(tempS);
+
+        // ID INPUT VALIDATION
+        System.out.print("Please enter an ID (8 integers): ");
+        tempS = input.nextLine();
+        while(!isNumeric(tempS) || tempS.length() != 8){
+            System.out.print("Invalid ID. Please enter an ID with 8 integers: ");
+            tempS = input.nextLine();
+        }
+        manager1.setID(tempS);  // Accepted if valid
+
+        // HOURLY RATE
+        System.out.print("Please enter an hourly rate: ");
+        tempS = input.nextLine();
+        while(!isNumeric(tempS)){
+            System.out.print("Invalid rate. Please make sure it is positive. ");
+            tempS = input.nextLine();
+        }
+        manager1.setHourlyPay(Double.parseDouble(tempS));   // Accepted if valid
+
+        // NUMBER OF HOURS
+        System.out.print("Please enter the number of REGULAR hours worked: ");
+        tempS = input.nextLine();
+        while(!isNumeric(tempS) || Double.parseDouble(tempS) > 40){
+            System.out.print("Invalid number. Please make sure it is positive "
+                    + "and no more than 40 hours. ");
+            tempS = input.nextLine();
+        }
+        manager1.setWorkHours(Double.parseDouble(tempS));   // Accepted if valid
+
+        // OVERTIME HOURLY RATE
+        System.out.print("Please enter an OVERTIME hourly rate: ");
+        tempS = input.nextLine();
+        while(!isNumeric(tempS)){
+            System.out.print("Invalid rate. Please make sure it is positive. ");
+            tempS = input.nextLine();
+        }
+        manager1.setOvertimePay(Double.parseDouble(tempS));   // Accepted if valid
+
+        // NUMBER OF OVERTIME HOURS (0-18)
+        System.out.print("Please enter the number of OVERTIME hours worked: ");
+        tempS = input.nextLine();
+        while(!isNumeric(tempS) || Double.parseDouble(tempS) > 18){
+            System.out.print("Invalid number. Please make sure it is positive "
+                    + "and no more than 18 hours. ");
+            tempS = input.nextLine();
+        }
+        manager1.setOvertimeHours(Double.parseDouble(tempS));   // Accepted if valid
+
+    }
+
+    /**********************************************
+     * These functions accept input from the user.
+     * Employees Function
+     **********************************************/
     private static void INPUT(ArrayList<Employee> e){
         // Creates a dummy variable to validate input
         String tempS;
@@ -114,7 +186,7 @@ public class payroll {
             tempS = input.nextLine();
             while(!isNumeric(tempS) || Double.parseDouble(tempS) > 40){
                 System.out.print("Invalid number. Please make sure it is positive "
-                        + "and below 40 hours. ");
+                        + "and no more than 40 hours. ");
                 tempS = input.nextLine();
             }
             e.get(i).setWorkHours(Double.parseDouble(tempS));   // Accepted if valid
@@ -148,37 +220,64 @@ public class payroll {
     * This function writes to an output file
     * to save the data.
     ***************************************/
-    private static void write(ArrayList<Employee> e){
+    private static void write(ArrayList<Employee> e, Manager manager1){
         // Defines variables
         String fileName = "output.txt";
 
-        try{
-            // Assume default encoding
-            FileWriter fileWriter = new FileWriter(fileName);
+        // Creates calendar and date variables and
+        // provides a standard American date format
+        Calendar calendar = Calendar.getInstance();
+        DateFormat df = new SimpleDateFormat("MM/dd/yy HH:mm:ss");
 
-            // Always wrap FileWriter in BufferedWriter
-            BufferedWriter writer = new BufferedWriter(fileWriter);
+
+        try{
+            // Always wrap FileWriter in PrintWriter
+            PrintWriter writer = new PrintWriter(new FileWriter(fileName, true));
+
+            // Begin by writing the date
+            writer.write(df.format(calendar.getTime()));
+            writer.println();
 
             // FOR MANAGER
+            writer.write("MANAGER'S Full Name: " + manager1.getFullName());
+            writer.println();
+            writer.write("Employee ID: " + manager1.getID());
+            writer.println();
+            writer.write("Hourly Wage " + manager1.getHourlyPay());
+            writer.println();
+            writer.write("Number of Hours Worked: " + manager1.getWorkHours());
+            writer.println();
+            writer.write("Regular Pay: " + manager1.calc());
+            writer.println();
+            writer.write("Overtime Wage " + manager1.getOvertimePay());
+            writer.println();
+            writer.write("Number of Overtime Hours Worked: " + manager1.getOvertimeHours());
+            writer.println();
+            writer.write("Overtime Pay: " + manager1.OverTimeCalc());
+            writer.println();
+            writer.write("Total Pay: " + manager1.totalCalc());
+            writer.println();
+            writer.println();   // Adds extra lines to the output file
 
             // FOR EMPLOYEES
             for (int counter = 0; counter < e.size(); counter++){
                 writer.write("Employee " + (counter+1) );
-                writer.newLine();
+                writer.println();
                 writer.write("Full Name: " + e.get(counter).getFullName());
-                writer.newLine();
+                writer.println();
                 writer.write("Employee ID: " + e.get(counter).getID());
-                writer.newLine();
+                writer.println();
                 writer.write("Hourly Wage " + e.get(counter).getHourlyPay());
-                writer.newLine();
+                writer.println();
                 writer.write("Number of Hours Worked: " + e.get(counter).getWorkHours());
-                writer.newLine();
+                writer.println();
                 writer.write("Total Pay: " + e.get(counter).calc());
-                writer.newLine();
+                writer.println();
+                writer.println();   // Adds an extra line to the output file
             }
             // Closes file
             writer.close();
-            System.out.println("\nData Saved!\n");
+            System.out.println("\nData Saved!\nOutput file was not overwritten!\n");
         }
         catch(IOException ex){
             System.out.println("Error writing to file '" + fileName + "'");
