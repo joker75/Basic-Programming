119db3dce02e2945f21c8bde12950f1820462dd3
Victor Nguyen <vdn140030@gmail.com>
2016-12-10 19:24:36 -0600

    Update Manager.java

--- payroll-system/Manager.java
+++ payroll-system/Manager.java
@@ -7,20 +7,22 @@
  *
  * Created by Victor on 12/8/2016.
  **************************************/
-public class Manager extends abstractPerson {
+class Manager extends abstractPerson {
     // Defines private data fields
     private double overtimeHourlyPay;   // How much pay per hour: validate for positive #'s
     private double overtimeWorkHours;   // How many hours: validate for positive #'s and
                                         // max time (40 hours) overtime (58 hours)
     // SETTER METHODS
-    public void setOvertimePay(double pay)  {this.overtimeHourlyPay = pay;}
-    public void setOvertimeHours(double hours) {this.overtimeWorkHours = hours;}
+    void setOvertimePay(double pay)  {this.overtimeHourlyPay = pay;}
+    void setOvertimeHours(double hours) {this.overtimeWorkHours = hours;}
 
     // GETTER METHODS
-    public double getOvertimePay()  {return this.overtimeHourlyPay;}
-    public double getOvertimeHours(){return this.overtimeWorkHours;}
+    double getOvertimePay()  {return this.overtimeHourlyPay;}
+    double getOvertimeHours(){return this.overtimeWorkHours;}
 
     // Calculates overtime pay
     // total pay = hours * rate
-    public double OverTimecalc()    {return this.overtimeHourlyPay * this.overtimeWorkHours;}
+    double OverTimeCalc()    {return this.overtimeHourlyPay * this.overtimeWorkHours;}
+    double totalCalc()       {return calc() + OverTimeCalc();}
+
 }
